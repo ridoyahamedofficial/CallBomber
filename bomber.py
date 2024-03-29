5e1082d073761002a6e81b7871efda976b72335f
Fojlu M <123924841+ridoyahamedofficial@users.noreply.github.com>
2024-03-29 15:58:24 +0600

    bomber.py

---
+++ bomber.py
@@ -0,0 +1,7 @@
+def send_unlimited_sms_to_bangladesh(number, message):
+    # Your code for sending SMS to Bangladeshi numbers goes here
+    pass
+
+def make_unlimited_calls_to_bangladesh(number):
+    # Your code for making calls to Bangladeshi numbers goes here
+    pass
